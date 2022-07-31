c902b16e44fda848c232cff146fbeb5dc97582f7
Xojibek777 <108388951+Xojibek777@users.noreply.github.com>
2022-07-31 13:52:39 +0500

    MATH

---
+++ math.py
@@ -0,0 +1,15 @@
+import math
+PI = math.pi
+print(f"PI ning qiymati: {PI}")
+E = math.e
+print(f"e ning qiymati: {E}")
+
+sin = math.sin(math.pi/2)
+cos = math.cos(0)
+tan = math.tan(PI)
+print(sin)
+print(cos)
+print(tan)
+
+print(math.degrees(math.pi/2))
+print(math.radians(90))
